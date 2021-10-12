ccdbdd7ff8dd71a2bd0925436c803659ec8db254
Twona <kadetweiler@gmail.com>
2021-10-12 14:08:56 -0500

    Add files via upload
    Update Hi-Low to new version with following known issue;
    - cardDraw is creating two cards of the exact same information (value, name, and suit)

--- Hi-Low.py
+++ Hi-Low.py
@@ -7,29 +7,23 @@ Money = 1000
 
 
 # Function to draw one random card
-class drawCard():
-    def __init__(self, cardDrawValue, cardName, cardDrawSuit):
-        self.cardDrawValue = randrange(1, 14)
-        self.cardSuits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        self.cardDrawSuit = random.sample(self.cardSuits, 4)
-        self.cardName = cardDrawValue
+class drawCard:
+    cardDrawValue = randrange(1, 14)
+    cardSuits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+    cardDrawSuit = random.sample(cardSuits, 1)
+    cardName = cardDrawValue
 
-    def drawthecard(self, cardDrawValue, cardName, cardDrawSuit):
-        cardDrawValue = randrange(1, 14)
-        cardSuits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        cardDrawSuit = random.sample(cardSuits, 4)
-        cardName = cardDrawValue
-        if cardDrawValue == 1:
-            cardName = "Ace"
-        elif cardDrawValue == 11:
-            cardName = "Jack"
-        elif cardDrawValue == 12:
-            cardName = "Queen"
-        elif cardDrawValue == 13:
-            cardName = "King"
-        else:
-            cardName = str(cardDrawValue)
-        return self.cardDrawValue, self.cardName, self.cardDrawSuit
+    # Changes card name if face card
+    if cardDrawValue == 1:
+        cardName = "Ace"
+    elif cardDrawValue == 11:
+        cardName = "Jack"
+    elif cardDrawValue == 12:
+        cardName = "Queen"
+    elif cardDrawValue == 13:
+        cardName = "King"
+    else:
+        cardName = str(cardDrawValue)
 
 
 def gameBegin(Money):
@@ -38,42 +32,36 @@ def gameBegin(Money):
         time.sleep(1)
 
         # Set default values for both cards
-        cardDrawValue1 = 0
-        cardName1 = ""
-        cardDrawSuit1 = ""
-        cardDrawValue2 = 0
-        cardName2 = ""
-        cardDrawSuit2 = ""
+        cardOne = drawCard
+        cardTwo = drawCard
 
         # Draw card and present it to player
-        drawCard(cardDrawValue1, cardName1, cardDrawSuit1)
-        print("Your first card is a " + cardName1 + " of " + cardDrawSuit1 + ".")
+        print(f"Your first card is a", cardOne.cardName, "of", str(cardOne.cardDrawSuit), ".")
 
         # Let player guess with loop to check input
-        input("Will you guess High (1) or Low(2):")
-        while input != 1 or input != 2:
-            input("Invalid entry. Please enter 1 for High or 2 for Low:")
+        choice = input("Will you guess High (1) or Low (2):")
+        while choice not in ["1", "2"]:
+            choice = input("Invalid choice. Please guess High (1) or Low (2):")
 
         # Draw second card and present it to player
-        drawCard(cardDrawValue2, cardName2, cardDrawSuit2)
-        print("your second card is a " + cardName2 + " of " + cardDrawSuit2 + ".")
+        print(f"Your second card is a", cardTwo.cardName, "of", str(cardTwo.cardDrawSuit), ".")
         time.sleep(1)
 
         # Logic to check card value and if player won or lost
-        if input == 1:
-            if cardDrawValue1 > cardDrawValue2:
+        if input == "1":
+            if cardOne.cardDrawValue > cardTwo.cardDrawValue:
                 Money += 5
                 print("You have won $5!")
-            elif cardDrawValue1 < cardDrawValue2:
+            elif cardOne.cardDrawValue < cardTwo.cardDrawValue:
                 Money -= 5
                 print("You lost $5.")
             else:
                 print("It's a tie. Your money will be returned.")
-        elif input == 2:
-            if cardDrawValue1 > cardDrawValue2:
+        elif input == "2":
+            if cardOne.cardDrawValue > cardTwo.cardDrawValue:
                 Money -= 5
                 print("You lost $5.")
-            elif cardDrawVale1 < cardDrawValue2:
+            elif cardOne.cardDrawVale < cardTwo.cardDrawValue2:
                 Money += 5
                 print("You have won $5.")
             else:
